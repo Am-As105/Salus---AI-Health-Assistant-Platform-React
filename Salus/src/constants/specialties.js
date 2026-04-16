881bebb5456ba1a75b5d4a2a594805ac94155f8f
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-16 11:19:43 +0100

    feat(constants): add specialties, time slots and consultation types

---
+++ Salus/src/constants/specialties.js
@@ -0,0 +1,22 @@
+export const SPECIALTIES = [
+  { value: '', label: '-- Choisir une spécialité --' },
+  { value: 'generaliste', label: 'Médecin généraliste' },
+  { value: 'cardiologue', label: 'Cardiologue' },
+  { value: 'dermatologue', label: 'Dermatologue' },
+  { value: 'pediatre', label: 'Pédiatre' },
+  { value: 'ophtalmologue', label: 'Ophtalmologue' },
+  { value: 'neurologue', label: 'Neurologue' },
+  { value: 'gynecologue', label: 'Gynécologue' },
+  { value: 'orthopediste', label: 'Orthopédiste' },
+]
+
+export const TIME_SLOTS = [
+  '08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
+  '11:00', '11:30', '14:00', '14:30', '15:00', '15:30',
+  '16:00', '16:30', '17:00', '17:30',
+]
+
+export const CONSULTATION_TYPES = [
+  { value: 'presentiel', label: '🏥 Présentiel' },
+  { value: 'teleconsultation', label: '💻 Téléconsultation' },
+]
